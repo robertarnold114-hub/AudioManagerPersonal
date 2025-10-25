0eea26d6715fe5bb07f2706f800df58fd7b5fc78
robertarnold114-hub <robert.arnold114@gmail.com>
2025-10-25 03:38:43 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false // ✅ ADD THIS
 }

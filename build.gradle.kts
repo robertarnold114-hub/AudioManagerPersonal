91314e1f9bedd816e57545b2f960c508843e1c30
robertarnold114-hub <robert.arnold114@gmail.com>
2025-10-25 00:42:15 -0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Root-level Gradle build file for AudioManagerPersonal
+
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}

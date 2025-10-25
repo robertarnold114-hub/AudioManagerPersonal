1e4ff7b45157fcccf082bdc3d8fd14c1f247ef4e
robertarnold114-hub <robert.arnold114@gmail.com>
2025-10-25 00:00:47 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.android") version "2.0.21"
 }
 android {
     namespace = "com.robertarnold.audiomanager"
